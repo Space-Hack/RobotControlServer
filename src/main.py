9304b5fe2346168cf8e0a5ee3d38362389806f04
root <noahgerber100@gmail.com>
2025-04-13 10:51:02 +0000

    add certs

--- src/main.py
+++ src/main.py
@@ -14,8 +14,12 @@ async def main():
     websocket_server = websockets.serve(handle_client, "localhost", 8765)
     print("🟢 WebSocket server running at ws://localhost:8765")
     
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    CERT_PATH = os.path.join(BASE_DIR, "certs", "cert.pem")
+    KEY_PATH = os.path.join(BASE_DIR, "certs", "cert-key.pem")
+
     # Start API server
-    config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+    config = uvicorn.Config(app, host="0.0.0.0", port=443, ssl_keyfile=KEY_PATH, ssl_certfile=CERT_PATH)
     server = uvicorn.Server(config)
     
     # Run both servers concurrently
